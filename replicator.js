317b9a294f3380b3d92095055aea04b6d4f4d5db
JRillette <JRillette@users.noreply.github.com>
2017-03-13 10:01:52 +0100

    v2
    Sort m4s and mp4 while sending to distrib servers

--- replicator.js
+++ replicator.js
@@ -1,30 +1,31 @@
-var fs = require('fs');
-var request = require('request');
 var express = require('express');
+var request = require('request');
+var fs = require('fs');
 var app = express();
+var file = './content/148469/out148469_dash1.m4s'
 
-var dst_file = './dst_file.txt';
-
-app.put('/api', function(req, res) {
-
-  var contentId = req.query.contentId;
-  var quality = req.query.quality;
-  var segment = req.query.segment;
-
-  // Simulation MPD
+app.get('/api/content/:id/:quality/:segment', function(req, res) {
+  var extension = req.params.segment.split(".")[1];
   var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(dst_file)
+      input: require('fs').createReadStream('./dstFile.txt')
   });
   lineReader.on('line', function (line) {
-    // Send m4s
-    console.log(line + '/api/content/'+contentId+"/"+quality+"/"+ segment);
-    fs.createReadStream(contentId+"/"+quality+"/"+segment).pipe(request.put(line +'/api/content/'+contentId+"/"+quality+"/"+ segment));
-    // req.pipe(request.put(line + '/api/content/'+contendId+"/"+quality+"/"+ segment));
-
+    console.log(line);
+    if (extension == 'mp4') {
+      // req.pipe(fs.createWriteStream('content.mp4')); pour quand Saad m'enverra les content
+      fs.createReadStream(file).pipe(request.put(line + '/api/mp4' + '/' + req.params.id + '/' + req.params.quality + '/' + req.params.segment));
+    } else if (extension == 'm4s') {
+      // req.pipe(fs.createWriteStream('content.m4s')); pour quand Saad m'enverra les content
+      fs.createReadStream(file).pipe(request.put(line + '/api/content' + '/' + req.params.id + '/' + req.params.quality + '/' + req.params.segment));
+    }
   });
-  res.end();
+  res.end('Aller la !!');
 });
-app.listen(8080);
 
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !!');
+});
 
-console.log('Replicator listening on 127.0.0.1:8080');
+app.listen(8080, '127.0.0.1')
+console.log('Rep_server listening on 127.0.0.1:8080');
