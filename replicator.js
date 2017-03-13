2f72b202b7f901135d0a9db4c194f5672ba98d54
Lwiip <louisgalava@gmail.com>
2017-03-13 13:27:12 +0100

    replicator v1 fonctionnel for m4s

--- replicator.js
+++ replicator.js
@@ -1,31 +1,69 @@
-var express = require('express');
-var request = require('request');
-var fs = require('fs');
+var express = require("express");
+var request = require("request");
+var fs = require("fs");
 var app = express();
-var file = './content/148469/out148469_dash1.m4s'
+//var file = "out500000_dash1.m4s";
 
-app.get('/api/content/:id/:quality/:segment', function(req, res) {
-  var extension = req.params.segment.split(".")[1];
-  var lineReader = require('readline').createInterface({
-      input: require('fs').createReadStream('./dstFile.txt')
+app.put("/api/content/:contentId/:quality/:segment", function(req, res) {
+  var contentId = req.params.contentId;
+  var quality = req.params.quality;
+  var segment = req.params.segment;
+
+//list destination
+  var lineReader = require("readline").createInterface({
+    input: require("fs").createReadStream("dst_file.txt")
   });
-  lineReader.on('line', function (line) {
+  lineReader.on("line", function(line) {
+    console.log(line +"/api/content" +"/" + contentId +"/" +quality +"/" +segment);
+
+    //Redirection
+    //var stream = req.pipe(request.put(line +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
+    var stream = req.pipe(request.put(line +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
+    stream.on("finish", function() {
+      res.end();
+    });
+
+  });
+
+/*
+  var stream = req.pipe(request.put("http://localhost:8081" +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
+    stream.on("finish", function() {
+      res.end();
+    });
+  */
+  //var stream = req.pipe(fs.createWriteStream(contentId+quality+segment));
+  
+    
+});
+
+app.post("/api/mp4/:id/:quality/:segment", function(req, res) {
+  var lineReader = require("readline").createInterface({
+    input: require("fs").createReadStream("dst_file.txt")
+  });
+  lineReader.on("line", function(line) {
     console.log(line);
-    if (extension == 'mp4') {
-      // req.pipe(fs.createWriteStream('content.mp4')); pour quand Saad m'enverra les content
-      fs.createReadStream(file).pipe(request.put(line + '/api/mp4' + '/' + req.params.id + '/' + req.params.quality + '/' + req.params.segment));
-    } else if (extension == 'm4s') {
-      // req.pipe(fs.createWriteStream('content.m4s')); pour quand Saad m'enverra les content
-      fs.createReadStream(file).pipe(request.put(line + '/api/content' + '/' + req.params.id + '/' + req.params.quality + '/' + req.params.segment));
-    }
+    fs
+      .createReadStream(file)
+      .pipe(
+        request.put(
+          line +
+            "/api/mp4" +
+            "/" +
+            req.params.id +
+            "/" +
+            req.params.quality +
+            "/" +
+            req.params.segment
+        )
+      );
   });
-  res.end('Aller la !!');
+  res.end("Aller la !!");
 });
 
-app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !!');
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "text/plain");
+  res.status(404).send("Page introuvable !!");
 });
 
-app.listen(8080, '127.0.0.1')
-console.log('Rep_server listening on 127.0.0.1:8080');
+app.listen(8000, "127.0.0.1");
+console.log("Rep_server listening on 127.0.0.1:8000");
