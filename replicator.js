bea86db1ce1d324eda7ffbddf20415c8e4188f1b
JRillette <JRillette@users.noreply.github.com>
2017-03-14 21:24:52 +0100

    Deal with mp4 and m4s

--- replicator.js
+++ replicator.js
@@ -14,50 +14,34 @@ app.put("/api/content/:contentId/:quality/:segment", function(req, res) {
     input: require("fs").createReadStream("dst_file.txt")
   });
   lineReader.on("line", function(line) {
-    console.log(line +"/api/content" +"/" + contentId +"/" +quality +"/" +segment);
+    console.log(line + "/api/content" + "/" + contentId + "/" + quality + "/" + segment);
 
     //Redirection
-    //var stream = req.pipe(request.put(line +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
-    var stream = req.pipe(request.put(line +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
+    var stream = req.pipe(request.put(line + "/api/content" + "/" + contentId + "/" + quality + "/" + segment));
     stream.on("finish", function() {
       res.end();
     });
-
   });
-
-/*
-  var stream = req.pipe(request.put("http://localhost:8081" +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
-    stream.on("finish", function() {
-      res.end();
-    });
-  */
-  //var stream = req.pipe(fs.createWriteStream(contentId+quality+segment));
-  
-    
 });
 
-app.post("/api/mp4/:id/:quality/:segment", function(req, res) {
+app.put("/api/mp4/:contentId/:quality/:segment", function(req, res) {
+  var contentId = req.params.contentId;
+  var quality = req.params.quality;
+  var segment = req.params.segment;
+
+  //list destination
   var lineReader = require("readline").createInterface({
     input: require("fs").createReadStream("dst_file.txt")
   });
   lineReader.on("line", function(line) {
-    console.log(line);
-    fs
-      .createReadStream(file)
-      .pipe(
-        request.put(
-          line +
-            "/api/mp4" +
-            "/" +
-            req.params.id +
-            "/" +
-            req.params.quality +
-            "/" +
-            req.params.segment
-        )
-      );
+    console.log(line + "/api/mp4" + "/" + contentId + "/" + quality + "/" + segment);
+
+    //Redirection
+    var stream = req.pipe(request.put(line + "/api/mp4" + "/" + contentId + "/" + quality + "/" +segment));
+    stream.on("finish", function() {
+      res.end();
+    });
   });
-  res.end("Aller la !!");
 });
 
 app.use(function(req, res, next) {
@@ -65,5 +49,5 @@ app.use(function(req, res, next) {
   res.status(404).send("Page introuvable !!");
 });
 
-app.listen(8000, "127.0.0.1");
-console.log("Rep_server listening on 127.0.0.1:8000");
+app.listen(9000);
+console.log("Rep_server listening on 9000");
