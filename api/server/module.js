7872e6d17c6c9c27ce2296ef0d9ebd719c8ad3f1
JRillette <JRillette@users.noreply.github.com>
2017-04-04 17:32:04 +0200

    read json and defult json

--- api/server/module.js
+++ api/server/module.js
@@ -5,54 +5,50 @@
 let fs = require("fs");
 let request = require("request");
 let express = require("express");
-
+let arr = {'1':['http://192.168.2.122:8087'], '2':['http://192.168.2.130:8087']}; // Default json
 
 module.exports = options => {
   let service = options.service;
   let router = express.Router();
 
+  router.post("/manage/location", function(req, res) {
+    console.log("rcv put");
+    arr = req.body;
+    // console.log(arr);
+    // console.log(arr[1][0]);
+  });
 
   router.put("/content/:contentId/:quality/:segment", function(req, res) {
-    console.log("ok put rcv");
     var contentId = req.params.contentId;
     var quality = req.params.quality;
     var segment = req.params.segment;
+    var dst = arr[contentId];
 
-    //list destination
-    var lineReader = require("readline").createInterface({terminal: false,input: require("fs").createReadStream("dst_file.txt")});
-    lineReader.on("line", function(line) {console.log(line + "/api/content" + "/" + contentId + "/" + quality + "/" + segment);
-
-      //Redirection
-      var stream = req.pipe(request.put(line +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
-      stream.on("finish", function() {
-        res.end();
-      });
+    //Redirection
+    for (let i = 0; i < dst.length; i++) {
+      console.log(line +"/api/content" +"/" + contentId +"/" +quality +"/" +segment);
+      var stream = req.pipe(request.put(dst[i] +"/api/content" +"/" +contentId +"/" +quality +"/" +segment));
+    }
+    stream.on("finish", function() {
+      res.end('res.end');
     });
   });
 
+  router.put("/mp4/:contentId/:quality/:segment", function(req, res) {
+    let contentId = req.params.contentId;
+    let quality = req.params.quality;
+    let segment = req.params.segment;
+    var dst = arr[contentId];
 
-    router.put("/mp4/:contentId/:quality/:segment", function(req, res) {
-        let contentId = req.params.contentId;
-        let quality = req.params.quality;
-        let segment = req.params.segment;
-
-        //list destination
-         var lineReader = require("readline").createInterface({terminal: false,
-          input: require("fs").createReadStream("dst_file.txt")
-         });
-         lineReader.on("line", function(line) {
-           console.log(line +"/api/mp4" +"/" + contentId +"/" +quality +"/" +segment);
-
-        //Redirection
-    
-        var stream = req.pipe(request.put(line +"/api/mp4" +"/" +contentId +"/" +quality +"/" +segment));
-        stream.on("finish", function() {
-            res.end();
-         });
-       });
+    //Redirection
+    for (let i = 0; i < dst.length; i++) {
+      console.log(line +"/api/mp4" +"/" + contentId +"/" +quality +"/" +segment);
+      var stream = req.pipe(request.put(dst[i] +"/api/mp4" +"/" +contentId +"/" +quality +"/" +segment));
+    }
+    stream.on("finish", function() {
+      res.end('res.end');
     });
-
-
+  });
 
   return router;
 };
